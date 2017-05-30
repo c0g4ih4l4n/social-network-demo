c41746f762699345b0407384e8d6028081245be7
te <longtranf@gmail.com>
2017-05-30 11:39:30 +0700

    Fixed Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,23 +21,40 @@
 
   <header>
     <div class="container">
-      <img src="{{ URL::asset('img/logo.png') }}" class="logo" alt="">
+      <a href="{{ URL::route('home') }}"><img src="{{ URL::asset('img/logo.png') }}" class="logo" alt=""></a>
+
+
+      @if (!isset($user))
       <form class="form-inline">
+
         <div class="form-group">
           <label class="sr-only" for="exampleInputEmail3">Email address</label>
           <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter email">
         </div>
+
         <div class="form-group">
           <label class="sr-only" for="exampleInputPassword3">Password</label>
           <input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
         </div>
+
         <button type="submit" class="btn btn-default">Sign in</button><br>
+
         <div class="checkbox">
           <label>
             <input type="checkbox"> Remember me
           </label>
         </div>
       </form>
+      @else
+      <div class="pull-right">
+      <ul class="nav nav-tabs">
+        <li class="presentation active"><a href="{{ URL::route('profile', $user->id) }}">Hello,{{ $user->name }}</a></li>
+        <li class="presentation"><a href="{{ URL::route('home') }}">Home</a></li>
+        <li class="presentation"><a href="{{ URL::route('logout') }}">Log out</a></li>
+      </ul>
+      </div>
+      @endif
+
     </div>
   </header>
 
